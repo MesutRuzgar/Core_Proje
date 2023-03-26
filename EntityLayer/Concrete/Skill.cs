df30ddbcf9b327ffa28ad3f46426c4d7238d81f7
MesutRuzgar <mesutruzgar@gmail.com>
2023-03-26 22:22:44 +0300

    key attibutelerin atamasi yapildi

--- EntityLayer/Concrete/Skill.cs
+++ EntityLayer/Concrete/Skill.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,7 +8,8 @@ using System.Threading.Tasks;
 namespace EntityLayer.Concrete
 {
     public class Skill
-    {   
+    {
+        [Key]
         public int SkillId { get; set; }
         public string Title { get; set; }
         public string Value { get; set; }
