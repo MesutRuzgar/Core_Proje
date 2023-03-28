98f8bbef901ef4ba46c8619d075ffc68a73f3a9a
MesutRuzgar <mesutruzgar@gmail.com>
2023-03-28 22:35:16 +0300

    entityframework entity dallari olusturuldu

---
+++ DataAccessLayer/EntityFramework/EfAboutDal.cs
@@ -0,0 +1,16 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Repository;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.EntityFramework
+{
+    public class EfAboutDal : GenericRepository<About>, IAboutDal
+    {
+        //IAboutDal eklememizin sebebi normal crud işlemleri dışında sadece bu entities'e ait  bir işlem gerçekleştirmek istersek imzasını IAboutDal'da atıp içini burada dolduracağız bu sayede bağımsız olacak.
+    }
+}
