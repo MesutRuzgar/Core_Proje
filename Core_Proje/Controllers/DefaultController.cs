ff8858cfc2c3abcbefe7128474c2d9dd8f4380c2
MesutRuzgar <mesutruzgar@gmail.com>
2023-07-17 20:17:10 +0300

    HeaderPartial olusturuldu ve aciklamalar ile kaydedildi

--- Core_Proje/Controllers/DefaultController.cs
+++ Core_Proje/Controllers/DefaultController.cs
@@ -8,9 +8,25 @@ namespace Core_Proje.Controllers
 {
     public class DefaultController : Controller
     {
+        //index e sağ tıklayıp viewden indexi buraya aldık en başta
         public IActionResult Index()
         {
             return View();
+        }
+
+        //parçalara bölüyoruz teker teker
+        //sağ tıklayıp add view de sonra add view de razor view tıkla (2.)
+        //Create as a partial view tikli olacak
+        //Head kısmında veya başka bir partial i çağırmak için ;
+        //@await Html.PartialAsync("HeaderPartial") kullanıyoruz. " " içerisine partiala verdiğimiz ismi yazıyoruz.
+        //index üzerinden çağırdığımız ve index ise bağlı bulunduğu controller içinde olduğundan mütevellit yukarıdaki
+        //gibi çağırıyoruz. Eğer ki kontroller farklı bir controller içersinde olsaydı " " içine onun view tarafındaki
+        //yolu yazılacaktı.
+        
+        //indexten head kısmını kes ve HeaderPartial'a sağ tıkla Go to View diyerek içerisine yapıştır. Artık Head kısmımız burası oldu.
+        public PartialViewResult HeaderPartial()
+        {
+            return PartialView();
         }
     }
 }
