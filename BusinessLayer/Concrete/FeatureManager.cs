6f2f35d650e7f9f5a65b8a9fb51ef4e4f9726bd5
MesutRuzgar <mesutruzgar@gmail.com>
2023-07-17 20:56:21 +0300

    FeatureList i cagirabilmek icin FeatureManager olusturuldu

---
+++ BusinessLayer/Concrete/FeatureManager.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class FeatureManager : IGenericService<Feature>
+    {
+        //constructor method unutma!!
+        IFeatureDal _featureDaL;
+
+        public FeatureManager(IFeatureDal featureDaL)
+        {
+            _featureDaL = featureDaL;
+        }
+
+        public void TAdd(Feature t)
+        {
+            _featureDaL.Insert(t);
+        }
+
+        public void TDelete(Feature t)
+        {
+            _featureDaL.Delete(t);
+        }
+
+        public Feature TGetById(int id)
+        {
+            return _featureDaL.GetById(id);
+        }
+
+        public List<Feature> TGetList()
+        {
+            return _featureDaL.GetList();
+        }
+
+        public void TUpdate(Feature t)
+        {
+            _featureDaL.Update(t);
+        }
+    }
+}
