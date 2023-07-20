8d2050a71cdebb2764b7e1ab05cf664a5fbc06c5
MesutRuzgar <mesutruzgar@gmail.com>
2023-07-20 22:49:50 +0300

    admin layout hazirlandi

---
+++ Core_Proje/Controllers/FeatureController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.Controllers
+{
+    public class FeatureController : Controller
+    {
+
+        //sag tıkla view ekle
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
