ef78789760a77318c1663a52cd00d1dc6832e837
MesutRuzgar <mesutruzgar@gmail.com>
2023-07-20 20:53:52 +0300

    bana ulasin kismi olusturuldu aktif EDILMEDI. send ve details olarak 2 parca olarak olusturuldu

---
+++ Core_Proje/ViewComponents/Contact/ContactDetail.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.ViewComponents.Contact
+{
+    public class ContactDetails : ViewComponent
+    {
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    
+    }
+}
