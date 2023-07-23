7b992623574202ea6c29f568cf488a8c71e2bce1
MesutRuzgar <mesutruzgar@gmail.com>
2023-07-23 21:16:25 +0300

    admin about update aktif edildi

---
+++ Core_Proje/Controllers/AboutController.cs
@@ -0,0 +1,36 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.Controllers
+{
+    public class AboutController : Controller
+    {
+        AboutManager aboutManager = new AboutManager(new EfAboutDal());
+
+       
+        public IActionResult Index()
+        {
+            ViewBag.d1 = "Düzenleme";
+            ViewBag.d2 = "Hakkımda";
+            ViewBag.d3 = "Hakkımda Sayfası";
+            //id 1 vermemizin sebebi feature managerda her zaman bir id ile calisacaz
+            //o id ise "1"
+            var values = aboutManager.TGetById(1);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult Index(About about)
+        {
+            aboutManager.TUpdate(about);
+            //default eklememizin sebebi default indexte calisacaz surekli
+            return RedirectToAction("Index", "Default");
+        }
+    }
+}
