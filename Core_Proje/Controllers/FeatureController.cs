0ece0024ee7b831a9e5e9f66e06f9435fb7dfe8f
MesutRuzgar <mesutruzgar@gmail.com>
2023-07-23 21:04:24 +0300

    admin panelinden feature duzenleme aktif edildi artik ana sayfada feature guncelleme yapilabiliyor

--- Core_Proje/Controllers/FeatureController.cs
+++ Core_Proje/Controllers/FeatureController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,11 +11,26 @@ namespace Core_Proje.Controllers
 {
     public class FeatureController : Controller
     {
+        FeatureManager featureManager = new FeatureManager(new EfFeatureDal());
 
         //sag tıkla view ekle
         public IActionResult Index()
         {
-            return View();
+            ViewBag.d1 = "Düzenleme";
+            ViewBag.d2 = "Öne Çıkanlar";
+            ViewBag.d3 = "Öne Çıkan Sayfası";
+            //id 1 vermemizin sebebi feature managerda her zaman bir id ile calisacaz
+            //o id ise "1"
+            var values = featureManager.TGetById(1);
+            return View(values);
+        }
+        
+        [HttpPost]
+        public IActionResult Index(Feature feature)
+        {
+            featureManager.TUpdate(feature);
+            //default eklememizin sebebi default indexte calisacaz surekli
+            return RedirectToAction("Index","Default");
         }
     }
 }
