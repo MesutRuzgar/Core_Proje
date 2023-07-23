65a9e94ce3ae1091185e9dff0d096a188f0f2169
MesutRuzgar <mesutruzgar@gmail.com>
2023-07-23 14:21:35 +0300

    deneyim ekleme olusturuldu

--- Core_Proje/Controllers/ExperienceController.cs
+++ Core_Proje/Controllers/ExperienceController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -19,6 +20,22 @@ namespace Core_Proje.Controllers
             ViewBag.d3 = "Deneyim Listesi";
             var values = experienceManager.TGetList();
             return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddExperience()
+        {
+            ViewBag.d1 = "Deneyim Ekleme";
+            ViewBag.d2 = "Deneyimler";
+            ViewBag.d3 = "Deneyim Ekleme";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddExperience(Experience experience)
+        {
+            experienceManager.TAdd(experience);           
+            return RedirectToAction("Index");
         }
     }
 }
