864264e1677164cced747ef5b4966869f9266cb6
MesutRuzgar <mesutruzgar@gmail.com>
2023-07-23 16:16:56 +0300

    portfolio ekleme aktif edildi

--- Core_Proje/Controllers/PortfolioController.cs
+++ Core_Proje/Controllers/PortfolioController.cs
@@ -1,5 +1,6 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -13,8 +14,28 @@ namespace Core_Proje.Controllers
         PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolioDal());
         public IActionResult Index()
         {
+            ViewBag.d1 = "Proje Listesi ";
+            ViewBag.d2 = "Projelerim";
+            ViewBag.d3 = "Proje Listesi";
             var values = portfolioManager.TGetList();
             return View(values);
         }
+
+        [HttpGet]
+        public IActionResult AddPortfolio()
+        {
+            ViewBag.d1 = "Proje Ekleme ";
+            ViewBag.d2 = "Projelerim";
+            ViewBag.d3 = "Proje Ekleme";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddPortfolio(Portfolio portfolio)
+        {
+            portfolioManager.TAdd(portfolio);
+            return RedirectToAction("Index");
+        }
+
     }
 }
