062f0fa07aba813f16c40193698fb7a8852ab168
MesutRuzgar <mesutruzgar@gmail.com>
2023-07-23 15:20:49 +0300

    portfolio sayfasi olusturuldu db de entity duzenlemesi yapildi

---
+++ Core_Proje/Controllers/PortfolioController.cs
@@ -0,0 +1,20 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolioDal());
+        public IActionResult Index()
+        {
+            var values = portfolioManager.TGetList();
+            return View(values);
+        }
+    }
+}
