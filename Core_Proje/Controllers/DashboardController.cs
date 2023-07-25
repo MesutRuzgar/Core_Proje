37623acf38cd7eba55e8026b6097f9bb9a2ad741
MesutRuzgar <mesutruzgar@gmail.com>
2023-07-25 21:36:03 +0300

    yeni tema paketi eklendi

---
+++ Core_Proje/Controllers/DashboardController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.d1 = "Dashboard";
+            ViewBag.d2 = "İstatistikler";
+            ViewBag.d3 = "İstatistik Sayfası";
+            return View();
+        }
+    }
+}
