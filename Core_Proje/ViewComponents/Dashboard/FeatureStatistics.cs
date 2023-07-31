770188c53668cf810beea1e57c46eb6e3dbd09ad
MesutRuzgar <mesutruzgar@gmail.com>
2023-07-31 20:48:47 +0300

    dashboardda feature ye istatistik kartlari duzenlendi

--- Core_Proje/ViewComponents/Dashboard/FeatureStatistics.cs
+++ Core_Proje/ViewComponents/Dashboard/FeatureStatistics.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,11 +9,15 @@ namespace Core_Proje.ViewComponents.Dashboard
 {
     public class FeatureStatistics : ViewComponent
     {
-
+        Context c = new Context();
 
         public IViewComponentResult Invoke()
         {
-            //var values = contactManager.TGetList();
+
+            ViewBag.c1 = c.Skills.Count();
+            ViewBag.c2 = c.Messages.Where(x=>x.Status==false).Count();
+            ViewBag.c3 = c.Messages.Where(x => x.Status == true).Count();
+            ViewBag.c4 = c.Experiences.Count();
             return View();
         }
 
