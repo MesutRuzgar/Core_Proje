a50e3e51512ab446258a6394350867151ef3d2a4
MesutRuzgar <mesutruzgar@gmail.com>
2023-07-31 21:01:23 +0300

    dashboard icin son 5 proje alani duzenlendi

---
+++ Core_Proje/ViewComponents/Dashboard/Last5Projects.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.ViewComponents.Dashboard
+{
+    public class Last5Projects : ViewComponent
+    {
+        PortfolioManager portfolioManager = new PortfolioManager(new EfPortfolioDal());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = portfolioManager.TGetList();
+            return View(values);
+        }
+
+    }
+}
