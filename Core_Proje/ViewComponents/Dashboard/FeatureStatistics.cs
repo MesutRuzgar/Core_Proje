d0221803b6fd6cff4345ad24ba5bfebcb051fe5c
MesutRuzgar <mesutruzgar@gmail.com>
2023-07-31 20:10:00 +0300

    dashboard feature alani icin component olusturuldu ve dashboard href leri duzeltildi

---
+++ Core_Proje/ViewComponents/Dashboard/FeatureStatistics.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.ViewComponents.Dashboard
+{
+    public class FeatureStatistics : ViewComponent
+    {
+
+
+        public IViewComponentResult Invoke()
+        {
+            //var values = contactManager.TGetList();
+            return View();
+        }
+
+    }
+}
+
