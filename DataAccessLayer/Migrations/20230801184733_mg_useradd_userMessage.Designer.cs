632d4314e3d28eb0354b89a41c8a4deda162dba5
MesutRuzgar <mesutruzgar@gmail.com>
2023-08-01 21:52:22 +0300

    dbSet ve migration ile db guncellendi

--- DataAccessLayer/Migrations/20230801184733_mg_useradd_userMessage.Designer.cs
+++ DataAccessLayer/Migrations/20230801184733_mg_useradd_userMessage.Designer.cs
@@ -4,14 +4,16 @@ using DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20230801184733_mg_useradd_userMessage")]
+    partial class mg_useradd_userMessage
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -296,8 +298,8 @@ namespace DataAccessLayer.Migrations
                     b.Property<string>("Name")
                         .HasColumnType("nvarchar(max)");
 
-                    b.Property<string>("Password")
-                        .HasColumnType("nvarchar(max)");
+                    b.Property<int>("Password")
+                        .HasColumnType("int");
 
                     b.Property<bool>("Status")
                         .HasColumnType("bit");
--- EntityLayer/Concrete/User.cs
+++ EntityLayer/Concrete/User.cs
@@ -15,7 +15,7 @@ namespace EntityLayer.Concrete
         public string Surname { get; set; }
         public string UserName { get; set; }
         public string Mail { get; set; }
-        public int Password { get; set; }
+        public string Password { get; set; }
         public string ImageUrl { get; set; }
         public bool Status { get; set; }
 
