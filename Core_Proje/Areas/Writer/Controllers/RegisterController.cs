d81a2c73f859a9f022c5924e1c28da466d5415c4
MesutRuzgar <mesutruzgar@gmail.com>
2023-08-03 21:58:37 +0300

    register form ve controller olusturuldu

---
+++ Core_Proje/Areas/Writer/Controllers/RegisterController.cs
@@ -0,0 +1,24 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class RegisterController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(User u)
+        {
+            return View();
+        }
+    }
+}
