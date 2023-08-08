463363a83970d5cc49904a815ebc1aee14741c8e
MesutRuzgar <mesutruzgar@gmail.com>
2023-08-08 21:06:38 +0300

    register controller tamamlandi

--- Core_Proje/Areas/Writer/Controllers/RegisterController.cs
+++ Core_Proje/Areas/Writer/Controllers/RegisterController.cs
@@ -1,5 +1,6 @@
 ﻿using Core_Proje.Areas.Writer.Models;
 using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -11,15 +12,56 @@ namespace Core_Proje.Areas.Writer.Controllers
     [Area("Writer")]
     public class RegisterController : Controller
     {
+        //userManager Identity ilye birlikte gelen manager
+        //readonly ile sadece okunabilir yaptık
+        private readonly UserManager<WriterUser> _userManager;
+
+        public RegisterController(UserManager<WriterUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
         [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
+
         [HttpPost]
-        public IActionResult Index(UserRegisterViewModel p)
+        //async method kullandiğimiz icin async yaptik burayida
+        public  async Task<IActionResult> Index(UserRegisterViewModel p)
         {
-           
+            if (ModelState.IsValid)
+            {
+                WriterUser w = new WriterUser()
+                {
+                    Name = p.Name,
+                    Surname = p.Surname,
+                    UserName = p.UserName,
+                    Email = p.Mail,
+                    ImageUrl = p.ImageUrl
+
+                };
+                //async değere atatık createasync ise yeni bir hesap olusturmak icin identity
+                //kutuphanesinden yararlanındı
+                var result = await _userManager.CreateAsync(w, p.Password);
+
+                if (result.Succeeded)
+                {
+                    //basarili olursa login e yonlendiriyoruz
+                    return RedirectToAction("Index", "Login");
+                }
+                else
+                {
+                    foreach(var item in result.Errors)
+                    {
+                        //yanlıs olursa hata aciklamasini göster dedik
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+
+            }
+         
             return View();
         }
     }
--- Core_Proje/Areas/Writer/Models/UserRegisterViewModel.cs
+++ Core_Proje/Areas/Writer/Models/UserRegisterViewModel.cs
@@ -8,8 +8,15 @@ namespace Core_Proje.Areas.Writer.Models
 {
     public class UserRegisterViewModel
     {
+        [Required(ErrorMessage = "Lütfen Adınızı Giriniz")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Lütfen  Soyadınızı Giriniz")]
+        public string Surname { get; set; }
+
         [Required(ErrorMessage ="Lütfen Kullanıcı Adını Giriniz")]
         public string UserName { get; set; }
+        [Required(ErrorMessage = "Lütfen Resim Ekleyiniz")]
+        public string ImageUrl { get; set; }
         [Required(ErrorMessage = "Lütfen Şifreyi Giriniz")]
         public string Password { get; set; }
         [Required(ErrorMessage = "Lütfen Şifreyi Tekrar Giriniz")]
