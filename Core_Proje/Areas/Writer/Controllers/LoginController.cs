df964d8ca5c4a84de52a0c7f1215616528b61ead
MesutRuzgar <mesutruzgar@gmail.com>
2023-08-09 21:17:14 +0300

    login controller ve login sayfasi olusturuldu

---
+++ Core_Proje/Areas/Writer/Controllers/LoginController.cs
@@ -0,0 +1,26 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.Areas.Writer.Controllers
+{
+    [Area("Writer")]
+    public class LoginController : Controller
+    {
+        private readonly SignInManager<WriterUser> _signInManager;
+
+        public LoginController(SignInManager<WriterUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
