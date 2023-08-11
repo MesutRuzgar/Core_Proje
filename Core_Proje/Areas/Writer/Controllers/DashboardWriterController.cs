679b153ed2f729adc123a637f2549150144ceea5
MesutRuzgar <mesutruzgar@gmail.com>
2023-08-11 21:35:08 +0300

    writer dashboard kontroller ve view eklendi

---
+++ Core_Proje/Areas/Writer/Controllers/DashboardWriterController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.Controllers
+{
+    public class DashboardWriterController : Controller
+    {
+        [Area("Writer")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
