2851184e1989ae964c45974f4f79e010eb703c35
MesutRuzgar <mesutruzgar@gmail.com>
2023-08-11 20:05:33 +0300

    profile sayfasi udate icin kontroller kodlandi

--- Core_Proje/Areas/Writer/Controllers/ProfileController.cs
+++ Core_Proje/Areas/Writer/Controllers/ProfileController.cs
@@ -1,8 +1,10 @@
-﻿using EntityLayer.Concrete;
+﻿using Core_Proje.Areas.Writer.Models;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -23,6 +25,38 @@ namespace Core_Proje.Areas.Writer.Controllers
             //sisteme otantike olan kullanıcının kullanıcı adına göre deger al
             var values = await _userManager.FindByNameAsync(User.Identity.Name);
             return View(values);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UserEditViewModel p)
+        {
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
+
+            if (p.Picture !=null)
+            {
+                //resmin kaynagini alıyoruz once
+                var resource = Directory.GetCurrentDirectory();
+                //uzantisini aliyoruz
+                var extension = Path.GetExtension(p.Picture.FileName);
+                //benzersiz bir resim adı olusturduk ve uzantidan gelen adi sonuna ekledik
+                var imagename = Guid.NewGuid()+extension;
+                //resmin kaydedilecegi yolu belirledik 
+                var savelocation = resource + "/wwwroot/userimage/" + imagename;
+                //yukarida ki kod bloklarini gerceklestirip resim dosyasını olusturduk
+                var stream = new FileStream(savelocation, FileMode.Create);
+                //streamdan gelen akis degerine resmi kopyaladik
+                await p.Picture.CopyToAsync(stream);
+                //kullanicinin imageurl si image nameden gelen deger olacak
+                user.ImageUrl = imagename;
+            }
+            user.Name = p.Name;
+            user.Surname = p.Surname;
+            var result = await _userManager.UpdateAsync(user);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Default");
+            }
+            return View();
         }
     }
 }
