2536a7eeefb500600e97f1ba9caf21650c39afbd
MesutRuzgar <mesutruzgar@gmail.com>
2023-08-14 20:11:08 +0300

    side bar revize edildi message kontroller ve gecici olarak bos index sayfasi olusturuldu

---
+++ Core_Proje/Areas/Writer/Controllers/MessageController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.Areas.Writer.Controllers
+{
+    public class MessageController : Controller
+    {
+        [Area("Writer")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
