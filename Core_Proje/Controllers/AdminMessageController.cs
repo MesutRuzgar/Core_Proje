3a3ba24dfcbce38c6af83b4956491aba45db8f13
MesutRuzgar <mesutruzgar@gmail.com>
2023-08-25 21:03:45 +0300

    admin message icin gelen giden kutusu sayfasi tasarlandi

---
+++ Core_Proje/Controllers/AdminMessageController.cs
@@ -0,0 +1,29 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.Controllers
+{
+    public class AdminMessageController : Controller
+    {
+        WriterMessageManager writerMessageManager = new WriterMessageManager(new EfWriterMessageDal());
+        public IActionResult ReceiverMessageList()
+        {
+            string p;
+            p = "deneme@deneme.com";
+            var values = writerMessageManager.GetListReceiverMessage(p);
+            return View(values);
+        }
+        public IActionResult SenderMessageList()
+        {
+            string p;
+            p = "deneme@deneme.com";
+            var values = writerMessageManager.GetListSenderMessage(p);
+            return View(values);
+        }
+    }
+}
