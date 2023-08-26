c3910ddb26ea46100e0e955b33fddc0515ac9d76
MesutRuzgar <mesutruzgar@gmail.com>
2023-08-26 16:49:44 +0300

    admin/socialmedia icin bussinesin eksik kodlari hazirlandi controller ve index olusturuldu

---
+++ Core_Proje/Controllers/SocialMediaController.cs
@@ -0,0 +1,54 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.Controllers
+{
+    public class SocialMediaController : Controller
+    {
+        SocialMediaManager socialMediaManager = new SocialMediaManager(new EfSocialMediaDal());
+
+        public IActionResult Index()
+        {
+            var values = socialMediaManager.TGetList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddSocialMedia()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddSocialMedia(SocialMedia socialMedia)
+        {
+            socialMediaManager.TAdd(socialMedia);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteSocialMedia(int id)
+        {
+            var values = socialMediaManager.TGetById(id);
+            socialMediaManager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult EditSocialMedia()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult EditSocialMedia(SocialMedia socialMedia)
+        {
+            socialMediaManager.TUpdate(socialMedia);
+            return RedirectToAction("Index");
+        }
+
+    }
+}
