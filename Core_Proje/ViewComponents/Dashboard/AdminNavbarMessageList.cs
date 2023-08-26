4b2abf09804fbfe444583ca944b65406ebb92191
MesutRuzgar <mesutruzgar@gmail.com>
2023-08-26 20:05:36 +0300

    admin/dashboard ust menu mesajlar kismi verilere gore yapildi

--- Core_Proje/ViewComponents/Dashboard/AdminNavbarMessageList.cs
+++ Core_Proje/ViewComponents/Dashboard/AdminNavbarMessageList.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +10,12 @@ namespace Core_Proje.ViewComponents.Dashboard
 {
     public class AdminNavbarMessageList :ViewComponent
     {
+        WriterMessageManager writerMessageManager = new WriterMessageManager(new EfWriterMessageDal());
         public IViewComponentResult Invoke()
         {
-            return View();
+            string p = "deneme@deneme.com";
+            var values = writerMessageManager.GetListReceiverMessage(p).OrderByDescending(x=>x.WriterMessageId).Take(3).ToList();
+            return View(values);
         }
     }
 }
