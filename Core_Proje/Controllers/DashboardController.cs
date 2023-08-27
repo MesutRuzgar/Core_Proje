8a668cbf5932e40850c6820d0209bf9249787e86
MesutRuzgar <mesutruzgar@gmail.com>
2023-08-27 15:48:06 +0300

    admin dashboard giris yapan adminin adini soyadini ve resmini otomatik cekiyor artık

--- Core_Proje/Controllers/DashboardController.cs
+++ Core_Proje/Controllers/DashboardController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,10 +11,19 @@ namespace Core_Proje.Controllers
 {
     public class DashboardController : Controller
     {
-        [Authorize(Roles = "Admin")]
-        public IActionResult Index()
+        private readonly UserManager<WriterUser> _userManager;
+
+        public DashboardController(UserManager<WriterUser> userManager)
         {
-            
+            _userManager = userManager;
+        }
+
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            ViewBag.v = values.Name + " " + values.Surname;
+            ViewBag.v2 = values.ImageUrl;
             return View();
         }
     }
