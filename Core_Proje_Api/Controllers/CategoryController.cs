8de2fc4e16e32cd67a76c878b3cff33080256b73
MesutRuzgar <mesutruzgar@gmail.com>
2023-08-27 13:38:53 +0300

    api icin category controller olusturuldu ve swaggerda denendi

---
+++ Core_Proje_Api/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using Core_Proje_Api.DAL.ApiContext;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult CategoryList()
+        {
+            var c = new Context();
+            return Ok(c.Categories.ToList());
+        }
+    }
+}
