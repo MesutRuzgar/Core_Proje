6c9c25d7ba3ed4d5b3651a42f73c5a9c54c09edc
MesutRuzgar <mesutruzgar@gmail.com>
2023-08-28 21:24:16 +0300

    anasayfa admin about icin resim ekleme kodlari yazildi viewmodel olusturuldu

--- Core_Proje/Controllers/AboutController.cs
+++ Core_Proje/Controllers/AboutController.cs
@@ -1,10 +1,14 @@
 ﻿using BusinessLayer.Concrete;
+using Core_Proje.Models;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -15,20 +19,53 @@ namespace Core_Proje.Controllers
     {
         AboutManager aboutManager = new AboutManager(new EfAboutDal());
 
-       
+
         public IActionResult Index()
         {
-          
+
             //id 1 vermemizin sebebi feature managerda her zaman bir id ile calisacaz
             //o id ise "1"
             var values = aboutManager.TGetById(1);
-            return View(values);
+            AboutEditViewModel model = new AboutEditViewModel();
+            model.Address = values.Address;
+            model.Age = values.Age;
+            model.Description = values.Description;
+            model.ImageUrl = values.ImageUrl;
+            model.Mail = values.Mail;
+            model.Phone = values.Phone;
+            model.Title = values.Title;
+            return View(model);
         }
 
         [HttpPost]
-        public IActionResult Index(About about)
+        public IActionResult Index(AboutEditViewModel p)
         {
-            aboutManager.TUpdate(about);
+            var values = aboutManager.TGetById(1);
+            if (p.Picture != null)
+            {
+                //resmin kaynagini alıyoruz once
+                var resource = Directory.GetCurrentDirectory();
+                //uzantisini aliyoruz
+                var extension = Path.GetExtension(p.Picture.FileName);
+                //benzersiz bir resim adı olusturduk ve uzantidan gelen adi sonuna ekledik
+                var imagename = Guid.NewGuid() + extension;
+                //resmin kaydedilecegi yolu belirledik 
+                var savelocation = resource + "/wwwroot/userimage/" + imagename;
+                //yukarida ki kod bloklarini gerceklestirip resim dosyasını olusturduk
+                var stream = new FileStream(savelocation, FileMode.Create);
+                //streamdan gelen akis degerine resmi kopyaladik
+                p.Picture.CopyToAsync(stream);
+                //kullanicinin imageurl si image nameden gelen deger olacak
+                p.ImageUrl = imagename;
+            }
+            values.Address = p.Address;
+            values.Age = p.Age;
+            values.Description = p.Description;
+            values.ImageUrl = p.ImageUrl;
+            values.Mail = p.Mail;
+            values.Phone = p.Phone;
+            values.Title = p.Title;
+            aboutManager.TUpdate(values);
             //default eklememizin sebebi default indexte calisacaz surekli
             return RedirectToAction("Index", "Default");
         }
---
+++ Core_Proje/Models/AboutEditViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.Models
+{
+    public class AboutEditViewModel
+    {
+        public int AboutId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Age { get; set; }
+        public string Mail { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string ImageUrl { get; set; }
+        public IFormFile Picture { get; set; }
+    }
+}
