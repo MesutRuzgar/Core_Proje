70dbe447c6c5e8ced881818cd2895238bfd1ec25
MesutRuzgar <mesutruzgar@gmail.com>
2023-08-30 20:27:02 +0300

    admin proje eklerken 2 farkli resim ekleme eklendi ana sitede portfolio resim src leri revize edildi db de gereksiz portfolio sutunlari silindi

--- Core_Proje/Controllers/PortfolioController.cs
+++ Core_Proje/Controllers/PortfolioController.cs
@@ -1,12 +1,15 @@
 ﻿using BusinessLayer.Concrete;
 using BusinessLayer.ValidationRules;
+using Core_Proje.Models;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
 using FluentValidation.Results;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -31,13 +34,35 @@ namespace Core_Proje.Controllers
         }
 
         [HttpPost]
-        public IActionResult AddPortfolio(Portfolio portfolio)
+        public async Task<IActionResult> AddPortfolio(PortfolioViewModel viewModel)
         {
-            PortfolioValidator validations = new PortfolioValidator();
-            ValidationResult result = validations.Validate(portfolio);
-            if (result.IsValid)
+            if (ModelState.IsValid)
             {
-                if (portfolio.Value>=100)
+                Portfolio portfolio = new Portfolio
+                {
+                    Name = viewModel.Name,
+                    ImageUrl = viewModel.ImageUrl,
+                    ProjectUrl = viewModel.ProjectUrl,
+                    ImageUrl2 = viewModel.ImageUrl2,
+                    Platform = viewModel.Platform,
+                    Price = viewModel.Price,
+                    Status = viewModel.Status,
+                    Value = viewModel.Value
+                };
+
+                if (viewModel.Picture != null)
+                {
+                    var imageName1 = await UploadImage(viewModel.Picture);
+                    portfolio.ImageUrl = imageName1;
+                }
+
+                if (viewModel.Picture2 != null)
+                {
+                    var imageName2 = await UploadImage(viewModel.Picture2);
+                    portfolio.ImageUrl2 = imageName2;
+                }
+
+                if (portfolio.Value >= 100)
                 {
                     portfolio.Status = true;
                 }
@@ -45,19 +70,17 @@ namespace Core_Proje.Controllers
                 {
                     portfolio.Status = false;
                 }
+
                 portfolioManager.TAdd(portfolio);
                 return RedirectToAction("Index");
             }
             else
             {
-                foreach (var item in result.Errors)
-                {
-                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
-                }
+                return View(viewModel);
             }
-            return View();
-            
         }
+
+
 
         public IActionResult DeletePortfolio(int id)
         {
@@ -104,6 +127,37 @@ namespace Core_Proje.Controllers
 
             return View(portfolio);
         }
+
+        private async Task<string> UploadImage(IFormFile picture)
+        {
+            if (picture != null)
+            {
+                var allowedExtensions = new[] { ".jpg", ".jpeg", ".png" }; // İzin verilen uzantılar
+                var extension = Path.GetExtension(picture.FileName).ToLower();
+
+                if (allowedExtensions.Contains(extension))
+                {
+                    var resource = Directory.GetCurrentDirectory();
+                    var imageName = Guid.NewGuid() + extension;
+                    var saveLocation = Path.Combine(resource, "wwwroot/projectimage", imageName);
+
+                    using (var stream = new FileStream(saveLocation, FileMode.Create))
+                    {
+                        await picture.CopyToAsync(stream);
+                    }
+
+                    return imageName;
+                }
+                else
+                {
+                    //throw new Exception bir istisna olusturmak icin kullanilir buradaki amacimiz hatayi göstermek
+                    throw new Exception("Geçersiz dosya uzantısı. Sadece JPG, JPEG ve PNG ve  uzantıları kabul edilir.");
+                }
+            }
+
+            return null;
+        }
+
 
     }
 }
--- Core_Proje/Models/PortfolioViewModel.cs
+++ Core_Proje/Models/PortfolioViewModel.cs
@@ -1,26 +1,23 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
-namespace EntityLayer.Concrete
+namespace Core_Proje.Models
 {
-    public class Portfolio
+    public class PortfolioViewModel
     {
-        [Key]
         public int PortfolioId { get; set; }
         public string Name { get; set; }
         public string ImageUrl { get; set; }
+        public IFormFile Picture { get; set; }
         public string ProjectUrl { get; set; }
         public string ImageUrl2 { get; set; }
+        public IFormFile Picture2 { get; set; }
         public string Platform { get; set; }
         public string Price { get; set; }
         public bool Status { get; set; }
-       
         public int Value { get; set; }
-
-
     }
 }
