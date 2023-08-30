f20f5acf8da387ed85f8dc030b986eadbb5e798a
MesutRuzgar <mesutruzgar@gmail.com>
2023-08-30 23:06:52 +0300

    admin refarans ekleme islemine resim ekleme eklendi

--- Core_Proje/Controllers/TestimonialController.cs
+++ Core_Proje/Controllers/TestimonialController.cs
@@ -1,10 +1,13 @@
 ﻿using BusinessLayer.Concrete;
+using Core_Proje.Models;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -47,10 +50,62 @@ namespace Core_Proje.Controllers
         }
 
         [HttpPost]
-        public IActionResult AddTestimonial(Testimonial testimonial)
+        public async Task<IActionResult> AddTestimonial(TestimonialViewModel p)
         {
+          
+            Testimonial testimonial = new Testimonial
+            {
+                ClientName = p.ClientName,
+                Company = p.Company,
+                Comment = p.Comment, 
+                ImageUrl=p.ImageUrl
+            };
+            if (p.Picture != null)
+            {
+                var imageName = await UploadImage(p.Picture);
+                testimonial.ImageUrl = imageName;
+            }
+
             testimonialManager.TAdd(testimonial);
             return RedirectToAction("Index");
         }
+
+
+        private async Task<string> UploadImage(IFormFile picture)
+        {
+            if (picture != null)
+            {
+                var allowedExtensions = new[] { ".jpg", ".jpeg", ".png" }; // İzin verilen uzantılar
+                var extension = Path.GetExtension(picture.FileName).ToLower();
+
+                if (allowedExtensions.Contains(extension))
+                {
+                    var resource = Directory.GetCurrentDirectory();
+                    var imageName = Guid.NewGuid() + extension;
+                    var saveLocation = Path.Combine(resource, "wwwroot/testimonialimage", imageName);
+
+                    using (var stream = new FileStream(saveLocation, FileMode.Create))
+                    {
+                        await picture.CopyToAsync(stream);
+                    }
+
+                    return imageName;
+                }
+                else
+                {
+                    //throw new Exception bir istisna olusturmak icin kullanilir buradaki amacimiz hatayi göstermek
+                    throw new Exception("Geçersiz dosya uzantısı. Sadece JPG, JPEG ve PNG ve  uzantıları kabul edilir.");
+                }
+            }
+
+            return null;
+        }
     }
+
 }
+
+
+
+
+
+
---
+++ Core_Proje/Models/TestimonialViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.Models
+{
+    public class TestimonialViewModel
+    {
+        public int TestimonialId { get; set; }
+        //referans adı için clientname kullandık
+        public string ClientName { get; set; }
+        public string Company { get; set; }
+        public string Comment { get; set; }
+        public string ImageUrl { get; set; }
+        public IFormFile Picture { get; set; }
+    }
+}
