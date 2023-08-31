4da69a9deb308e371977dc169e48704431794c20
MesutRuzgar <mesutruzgar@gmail.com>
2023-08-31 22:12:42 +0300

    admin navbar mesaj listesine resimler getirildi gonderene gore resim gosteriyor kullanici resmi yok ise default resim gosteriyor

---
+++ Core_Proje/Models/UserMessageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.Models
+{
+    public class UserMessageViewModel
+    {
+        public int WriterMessageId { get; set; }
+        public string SenderName { get; set; }
+        public string MessageContent { get; set; }
+        public DateTime Date { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
--- Core_Proje/ViewComponents/Dashboard/AdminNavbarMessageList.cs
+++ Core_Proje/ViewComponents/Dashboard/AdminNavbarMessageList.cs
@@ -1,5 +1,9 @@
 ﻿using BusinessLayer.Concrete;
+using Core_Proje.Models;
+using DataAccessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -8,14 +12,36 @@ using System.Threading.Tasks;
 
 namespace Core_Proje.ViewComponents.Dashboard
 {
-    public class AdminNavbarMessageList :ViewComponent
+    public class AdminNavbarMessageList : ViewComponent
     {
         WriterMessageManager writerMessageManager = new WriterMessageManager(new EfWriterMessageDal());
         public IViewComponentResult Invoke()
         {
+            Context c = new Context();
             string p = "deneme@deneme.com";
-            var values = writerMessageManager.GetListReceiverMessage(p).OrderByDescending(x=>x.WriterMessageId).Take(3).ToList();
-            return View(values);
+            var values = writerMessageManager.GetListReceiverMessage(p).OrderByDescending(x => x.WriterMessageId).Take(3).ToList();
+            //list olusturmamizin sebebi yukarida liste almamiz
+            List<UserMessageViewModel> messageViewModels = new List<UserMessageViewModel>();
+            foreach (var item in values)
+            {
+                var user = c.Users.Where(x => x.Email == item.Sender).FirstOrDefault();
+                var model = new UserMessageViewModel
+                {
+                    ImageUrl = user.ImageUrl,
+                    Date = item.Date,
+                    MessageContent = item.MessageContent,
+                    SenderName = item.SenderName,
+                    WriterMessageId = item.WriterMessageId,
+                };
+                messageViewModels.Add(model);
+
+            }
+            return View(messageViewModels);
         }
+
+
+
     }
+
 }
+
