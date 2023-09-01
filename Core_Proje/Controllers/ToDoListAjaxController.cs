6888cdd262a6983ff6e2f2783f84c85ee5e13ec4
MesutRuzgar <mesutruzgar@gmail.com>
2023-09-01 21:26:28 +0300

    admin dashboardda son 5 proje kaldırıldı. todolisticin ajax iceren kontroller yazildi ve viewcomponentten o kontrollera ekleme istegi atildi

---
+++ Core_Proje/Controllers/ToDoListAjaxController.cs
@@ -0,0 +1,34 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_Proje.Controllers
+{
+    public class ToDoListAjaxController : Controller
+    {
+        ToDoListManager toDoManager = new ToDoListManager(new EfToDoListDal());
+      
+        [HttpPost]
+        public IActionResult Add(ToDoList p)
+        {
+            p.Status = false;
+            toDoManager.TAdd(p);
+            var values = JsonConvert.SerializeObject(p);
+            return Json(values);
+        }
+     
+        public IActionResult Delete(int id)
+        {
+            var values = toDoManager.TGetById(id);
+            toDoManager.TDelete(values);
+            return NoContent();
+        }
+               
+    }
+}
