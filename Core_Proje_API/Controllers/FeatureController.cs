a0bba350f4857bee3f923685a1b0e0b67a1e4080
MesutRuzgar <mesutruzgar@gmail.com>
2023-09-03 16:59:53 +0300

    feature api controller tamamlandi

--- Core_Proje_API/Controllers/FeatureController.cs
+++ Core_Proje_API/Controllers/FeatureController.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -20,11 +21,49 @@ namespace Core_Proje_API.Controllers
         }
 
         [HttpGet("getall")]
-        public ActionResult GetAll()
+        public IActionResult GetAll()
         {
-            var values =_featureService.TGetList();
+            var values = _featureService.TGetList();
             return Ok(values);
+
+        }
+        [HttpGet("getid")]
+        public IActionResult GetById(int id)
+        {
+            var values = _featureService.TGetById(id);
+            return Ok(values);
+
+        }
+        [HttpPost("add")]
+        public IActionResult AddFeature(Feature feature)
+        {
+            _featureService.TAdd(feature);
+            return Ok(new { message = "Başarıyla eklendi." });
+
+        }
+        [HttpPost("update")]
+        public IActionResult UpdateFeature(Feature feature)
+        {
+            _featureService.TUpdate(feature);
+            return Ok(new { message = "Başarıyla güncellendi." });
+
+        }
+        [HttpDelete("delete")]
+        public IActionResult DeleteFeature(int id)
+        {
+
+            var delete = _featureService.TGetById(id);
+            if (delete == null)
+            {
+                return NotFound(new { message = "Belirtilen özellik bulunamadı." });
+            }
+            else
+            {
+                _featureService.TDelete(delete);
+                return Ok(new { message = "Başarıyla silindi." });
+            }
 
         }
     }
 }
+
