b1beb8c2a3740882fa1a24d3649aea39bd56da7d
MesutRuzgar <mesutruzgar@gmail.com>
2023-09-03 13:19:06 +0300

    admin dashboard okunmamislar kartı revize edildi script tanimlandi liste haline donduruldu

--- Core_Proje/ViewComponents/Dashboard/FeatureStatistics.cs
+++ Core_Proje/ViewComponents/Dashboard/FeatureStatistics.cs
@@ -15,12 +15,20 @@ namespace Core_Proje.ViewComponents.Dashboard
         {
 
             ViewBag.c1 = c.Skills.Count();
+            //okunmamıslar
             var m1 = c.Messages.Where(x => x.Status == false).Count();
             var m2 = c.WriterMessages.Where(x => x.Status == false).Count();
             ViewBag.c2 = m1 + m2;
+            //okunmuslar
             var m3 = c.Messages.Where(x => x.Status == true).Count();
-            var m4 = c.WriterMessages.Where(x => x.Status == true).Count();
+            var m4 = c.WriterMessages.Where(x => x.Status == true).Count();          
             ViewBag.c3 = m3 + m4;
+
+            //sayfadan gelen okunmamıs
+            ViewBag.y = m1;
+            //yazardan gelen okunmamıs
+            ViewBag.y1 = m2;
+
             ViewBag.c4 = c.Experiences.Count();
             return View();
         }
